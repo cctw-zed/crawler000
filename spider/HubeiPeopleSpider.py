54c30bfe5913f63f9ff25a6158b5520a75dfe012
kitaharakasusa <894655469@qq.com>
2020-04-14 17:45:29 +0800

    uuuuu

--- spider/HubeiPeopleSpider.py
+++ spider/HubeiPeopleSpider.py
@@ -5,15 +5,15 @@ import requests
 import urllib.request
 import re
 
-class BeijingSpider(object):
+class HubeiSpider(object):
     def __init__(self, keyword, pageNum=3, pageSize=10):
         self.headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Encoding': 'deflate, br',
         'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
         'Connection': 'keep-alive',
-        'Host': 'sousuo.bjrd.gov.cn:8080',
         'Upgrade-Insecure-Requests': '1',
+        'Host': 'www.hppc.gov.cn',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
     }
         self.keyword = keyword
@@ -21,25 +21,32 @@ class BeijingSpider(object):
         self.pageSize = pageSize
         self.connection = MyMongoDB()
         
-  
+    def getContent(self, pageurl):
+        try:
+            rep = requests.get(pageurl, headers=self.headers)
+            if rep.status_code == 200:
+                try:
+                    soup = BeautifulSoup(rep.text, 'lxml')
+                    print(soup)
+                    print(rep.encoding)
+                    text = soup.find('div', attrs={'id': 'endtext'}).text.encode(rep.encoding).decode('utf-8')
+                    return text
+                except:
+                    print('解析出错')
+                    return ''
+
+        except:
+            print('出错了')
+            return ''
+
     def getPage(self, pageIndex):
-        url = 'http://sousuo.bjrd.gov.cn:8080/was5/web/search'
+        url = 'http://www.hppc.gov.cn/list.php'
         params = {
-            'page': pageIndex,
-            'channelid': 237169,
-            'searchword': self.keyword,
-            'keyword': self.keyword,
-            'orderby': '-docreltime',
-            'token': '9.1350302879613.25',
-            'perpage': self.pageSize,
-            'outlinepage': 10,
-            'andsen': '',
-            'total': '',
-            'orsen': '',
-            'exclude': '',
-            'timescope': '',
-            'timescopecolumn':'', 
-            'orderby': '-docreltime'
+            'eyword': self.keyword,
+            'catid': 54,
+            'x': 20,
+            'y': 10,
+            'page':pageIndex,
         }
         response = requests.get(url, headers=self.headers, params=params)
         if(response.status_code ==200):
@@ -47,24 +54,22 @@ class BeijingSpider(object):
             self.parserPage(page)
         # print(page)
     def parserPage(self,page):
-        soup = BeautifulSoup(page,'lxml').find('div',  attrs={'class': 'search_con'})
-        titles = soup.find_all('p', attrs={'class': 'search_con_title'})
-        contents = soup.find_all('p', attrs={'class': 'search_con_txt'})
-        dates = soup.find_all('p', attrs={'class': 'search_con_date'})
+        soup = BeautifulSoup(page,'lxml')
+        # print(soup)
+        titles = soup.find_all('div',  attrs={'class': 'list1'})
         for i in range(len(titles)):
             try:
-                title = titles[i].text
-                hrefurl = titles[i].find('a').get('href')
-                if hrefurl == '':
-                    continue
-                content = contents[i].text
-                date = re.findall(r'\t(.+?)\n',dates[i].text)[0].strip()
+                title = titles[i].find('a').text
+
+                hrefurl = 'http://www.hppc.gov.cn'+ titles[i].find('a').get('href')
+                time = titles[i].find('span').text
+
                 res = {}
-                res['title'] = title.strip()
+                res['title'] = title
                 res['real_url'] = hrefurl
-                res['abstract'] = content.strip()
-                res['time'] = date
-                res['site'] = '北京市人大网'
+                res['abstract'] = self.getContent(hrefurl)
+                res['time'] = time
+                res['site'] = '湖北人大网'
                 res['keyword'] = self.keyword
                 # self.connection.insert(res)
                 print(res)
@@ -76,9 +81,10 @@ class BeijingSpider(object):
             self.getPage(i)
 
 if __name__ == "__main__":
-    spider = BeijingSpider('大火')
-    spider.run()
-
+    spider = HubeiSpider('大火')
+    # spider.run()
+    page = spider.getContent('http://www.hppc.gov.cn/2020/0413/33001.html')
+    print(page)
 
 
  
