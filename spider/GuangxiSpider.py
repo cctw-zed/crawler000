d67419465c69474e1f6b8607c8a82ae983273f92
Zed Zhao <u201413658@alumni.hust.edu.cn>
2020-04-16 20:05:34 +0800

    修改几个网站的名字为相关网站

---
+++ spider/GuangxiSpider.py
@@ -0,0 +1,98 @@
+import re
+import time
+from urllib.parse import urljoin
+
+import ConnectMongoDB
+import requests
+from bs4 import BeautifulSoup
+
+
+class GuangxiSpider(object):
+
+    def __init__(self):
+        # self.connection = ConnectMongoDB()
+        self.headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
+        }
+        self.base_url = 'https://www.gxrd.gov.cn'
+
+    # 抓取url得到response
+    def crawl(self, url, seen):
+        seen.add(url)
+        response = requests.get(url,headers=self.headers)
+        if response.status_code == 200:
+            return response
+    # 解析网页中包含的所有url
+    def parse(self, response, unseen, seen):
+        soup = BeautifulSoup(response.text, 'lxml')
+        urls = soup.find_all('a', {"href": re.compile('^/html/*')})
+        # print(urls)
+        page_urls = set()
+        for url in urls:
+            # title = url.get_text()
+            full_url = urljoin(self.base_url, url['href'])
+            if re.match("/html/art*", url['href']):
+                page_urls.add(full_url)
+            else:
+                unseen.add(full_url)
+        print(page_urls)
+            # print(unseen)
+        page_urls.update(page_urls - seen)
+        unseen.update(unseen - seen)
+        return page_urls
+
+    def artParse(self, response, url):
+        soup = BeautifulSoup(response.text, 'lxml')
+        content = soup.find('meta', {"name":"description"})['content']
+        # time = soup.find('p', {"class":"text-center a_inf lh30"}).get_text().split("&nbsp")[1]
+        # print(time)
+        title = soup.find('h2').find('span').get_text()
+        site = '广西人大网'
+        res = {}
+        res['title'] = title
+        res['real_url'] = url
+        res['abstract'] = content
+        # res['time'] = time
+        res['site'] = '辽宁人大网'
+        return res
+
+if __name__ == '__main__':
+    Spider = GuangxiSpider()
+    unseen = set([Spider.base_url, ])
+    seen = set()
+
+    count, t1 = 1, time.time()
+
+    while len(unseen) != 0:
+        print('\nDistributed Crawling...')
+
+        responses = []
+        for url in unseen:
+            responses.append(Spider.crawl(url,seen))
+            time.sleep(1)
+
+        print('\nDistributed Parsing...')
+        for response in responses:
+            try:
+                page_urls = Spider.parse(response, unseen, seen)
+                time.sleep(1)
+                # print(page_urls)
+
+                for url in page_urls:
+                    try:
+                        print(Spider.artParse(Spider.crawl(url, seen), url))
+                        time.sleep(1)
+                        count += 1
+                        print(count)
+                        print(time.time() - t1)
+                    except:
+                        continue
+            except:
+                continue
+
+print('Total time: %.1f s' % (time.time() - t1,))  # 53 s
