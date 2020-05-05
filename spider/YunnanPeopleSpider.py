cbb6b91910394cabef6b1364671335cc37d240b0
Zed Zhao <u201413658@alumni.hust.edu.cn>
2020-05-05 08:18:45 +0800

    Merge branch 'master' of https://github.com/cctw-zed/crawler000
    # Conflicts:
    #	spider/HeilongjiangSpider.py
    #	spider/__pycache__/ConnectMongoDB.cpython-37.pyc
    #	spider/__pycache__/MyBloom.cpython-37.pyc

---
+++ spider/YunnanPeopleSpider.py
@@ -0,0 +1,97 @@
+'''
+1. 将需要爬取的文章列表页面放进待爬取列表
+2. 对于每类页面，遍历
+'''
+from time import sleep
+from ToDataBase import ToDataBase
+import requests
+from ConnectMongoDB import MyMongoDB
+from bs4 import BeautifulSoup
+
+class YunNanSpider(object):
+
+    def __init__(self):
+        self.headers = {
+            'Accept':'text/css,*/*;q=0.1',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Connection': 'keep-alive',
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0'
+        }
+        # self.connection = MyMongoDB()
+        # self.ToDataBase("云南人大网")
+        self.baseUrl = 'http://www.srd.yn.gov.cn/'
+        self.urlList = {
+            'rdyw/',
+            'ddjs/gzbs/', 'ddjs/xxdt/','ddjs/xxjy/',
+            'zyfb/cwhgg/', 'zyfb/tzgs/','lfgz/lfdt/',
+            'lfgz/syhd/', 'jdgz/gzjd/', 'jdgz/fljd/',
+            'jdgz/ysjd/', 'jdgz/jhjd/', 'rsrm/',
+            'dbgz/gzdt', 'dbgz/dbfc/', 'dbgz/xxpx',
+            'ztjz/', 'jggz/jgjsgzdt/', 'jggz/jgjsxxpx/',
+            'jggz/jggzfpgz/', 'jggz/bwcxljsm/', 'jggz/bwcxljsm/zybs/',
+            'jggz/bwcxljsm/ynxd/', 'jggz/bwcxljsm/jgdt/', 'jggz/bwcxljsm/wlsp/',
+            'gzyj/', 'fzxc/',
+        }
+
+
+    def getResponse(self):
+        # 对于列表中的所有url
+        for url in self.urlList:
+            fullUrl = self.baseUrl + url
+            print(fullUrl)
+            # pages存放所有存放文章列表的页面
+            pages = []
+            firstPage = requests.get(fullUrl,headers=self.headers)
+            if firstPage.status_code == 200:
+                pages.append(firstPage)
+            else:
+                continue
+            index = 1
+            while True:
+                sleep(0.3)
+                tail = 'index_' + str(index) + '.html'
+                page = requests.get(fullUrl + tail)
+                print(page)
+                if page.status_code == 200:
+                    pages.append(page)
+                    index += 1
+                else:
+                    break;
+            print('-----')
+            self.parseResponse(pages,fullUrl)
+
+    def parseResponse(self, pages, fullUrl):
+        for page in pages:
+            page.encoding = "utf-8"
+            soup = BeautifulSoup(page.text, 'lxml')
+            # print(soup)
+            lis = soup.find('ul', attrs={'class':'newsline1'}).findAll('li')
+            for li in lis:
+                sleep(0.3)
+                a = li.find('a')
+                articleUrl = self.baseUrl + a['href'][2:]
+                # if not self.ToDataBase.Isexist(articleUrl):
+                res = {}
+                res['title'] = a.get_text()
+                res['real_url'] = articleUrl
+                res['abstract'] = res['title']
+                res['time'] = li.find('span').get_text()
+                res['site'] = '云南人大网'
+                print(res)
+                    # self.ToDataBase.Insert(res)
+
+    # def parseArt(self, articleUrl):
+    #     response = requests.get(articleUrl,headers=self.headers)
+    #     soup = BeautifulSoup(response.text, 'lxml')
+    #     div = soup.find('div', attrs={'class':'TRS_Editor'})
+    #     p = div.find('p')
+    #     return p.get_text()
+
+    def run(self):
+        self.getResponse()
+        # self.ToDataBase.End()
+
+if __name__ == "__main__":
+    spider = YunNanSpider()
+    spider.run()
