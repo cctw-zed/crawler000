91e047cc536517c895657532b09f483f030979c2
Zed Zhao <u201413658@alumni.hust.edu.cn>
2020-05-08 10:13:41 +0800

    update .gitignore

--- Search/search/app/models/working.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const WorkingSchema = mongoose.Schema({
-	keyword: String
-});
-
-// 用于保存当前是否在爬取的关键字表
-module.exports = mongoose.model('Working', WorkingSchema);
--- Search/search/config/database.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // 数据库连接的url
-    // 最后一个代表：数据库的名称 crawlSpider
-    url: 'mongodb://localhost:27017/crawlSpider'
-}
