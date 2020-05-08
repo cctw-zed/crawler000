5dee15622834ef9f8191b77b19fda527182e09e0
White Blue <1320156388@qq.com>
2020-05-08 12:00:41 +0800

    allcansee

--- spider/AllDataSpider/AllData_API.py
+++ spider/AllDataSpider/AllData_API.py
@@ -124,4 +124,4 @@ api.add_resource(Search, '/search/api')
 api.add_resource(Canbegin, "/canbegin/api")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
